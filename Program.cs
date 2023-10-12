1ac30c0c542d80e92c6feca3d81893ac3b4017ab
F1ejm <karolosrodka@gmail.com>
2023-10-12 21:35:46 +0200

    Add gameplay

--- Program.cs
+++ Program.cs
@@ -1,4 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Let's play rock-paper-scssors!");
+﻿Console.WriteLine("Let's play rock-paper-scissors!");
+
+Console.WriteLine("Provide sign, first player:");
+
+
+string? firstPlayerSign = Console.ReadLine();
+
+Console.WriteLine("Provide sign, second player:");
+ 
+string? secondPlayerSign = Console.ReadLine();
+
+if (firstPlayerSign == secondPlayerSign)  
+{
+    Console.WriteLine("It is a draw");
+}
+
+else if (firstPlayerSign == "rock" && secondPlayerSign == "scissors"  )
+{
+    Console.WriteLine("First player won");
+}
+else if (firstPlayerSign == "scissors" && secondPlayerSign == "paper"  )
+{
+    Console.WriteLine("First player won");
+}
+else if (firstPlayerSign == "paper" && secondPlayerSign == "rock"  )
+{
+    Console.WriteLine("First player won");
+}
+ 
+else 
+{
+    Console.WriteLine("Second player won");
+}
 Console.WriteLine("Press ENTER to close the game");
 Console.ReadLine();
