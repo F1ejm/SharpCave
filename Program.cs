8909b2cc483fb39ae2d4b9716d6439b5dc9e1b2a
F1ejm <karolosrodka@gmail.com>
2023-10-13 22:49:24 +0200

    Loop gameplayand quit

--- Program.cs
+++ Program.cs
@@ -1,35 +1,41 @@
 ﻿Console.WriteLine("Let's play rock-paper-scissors!");
 
-Console.WriteLine("Provide sign, first player:");
-
-
-string? firstPlayerSign = Console.ReadLine();
-
-Console.WriteLine("Provide sign, second player:");
- 
-string? secondPlayerSign = Console.ReadLine();
-
-if (firstPlayerSign == secondPlayerSign)  
+while (true)
 {
-    Console.WriteLine("It is a draw");
-}
+    Console.WriteLine("Provide sign, first player or write 'quit' to end game:");
+    string? firstPlayerSign = Console.ReadLine();
 
-else if (firstPlayerSign == "rock" && secondPlayerSign == "scissors"  )
-{
-    Console.WriteLine("First player won");
-}
-else if (firstPlayerSign == "scissors" && secondPlayerSign == "paper"  )
-{
-    Console.WriteLine("First player won");
-}
-else if (firstPlayerSign == "paper" && secondPlayerSign == "rock"  )
-{
-    Console.WriteLine("First player won");
-}
- 
-else 
-{
-    Console.WriteLine("Second player won");
+    if (firstPlayerSign == "quit")
+    {
+        break;
+    }
+    Console.WriteLine("Provide sign, second player or write 'quit' to end game:");
+    string? secondPlayerSign = Console.ReadLine();
+    if (secondPlayerSign == "quit")
+    {
+        break;
+    }
+    if (firstPlayerSign == secondPlayerSign)  
+    {
+        Console.WriteLine("It is a draw");
+    }
+    else if (firstPlayerSign == "rock" && secondPlayerSign == "scissors"  )
+    {
+        Console.WriteLine("First player won");
+    }
+    else if (firstPlayerSign == "scissors" && secondPlayerSign == "paper"  )
+    {
+        Console.WriteLine("First player won");
+    }
+    else if (firstPlayerSign == "paper" && secondPlayerSign == "rock"  )
+    {
+        Console.WriteLine("First player won");
+    }
+    
+    else 
+    {
+        Console.WriteLine("Second player won");
+    }
 }
 Console.WriteLine("Press ENTER to close the game");
 Console.ReadLine();
